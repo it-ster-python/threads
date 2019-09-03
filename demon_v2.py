9fd76f30e6134660fda86057206b4ef4dd6f81a2
Ваше Имя <hacker@google.com>
2019-09-03 20:10:45 +0300

    Demon v2

--- demon_v2.py
+++ demon_v2.py
@@ -4,47 +4,99 @@ import time
 from datetime import datetime
 from random import randint
 import signal
+import psutil
+# import multiprocessing as mp
 
 
-PID_FILE = ""
-
+PID_FILE = "/var/run/step/demon/demon.pid"
+WORK = True
 
 def demon():
 
     def handler(signum, frame):
         with open("signal.log", "a") as log_file:
-            log_file.write(f"Signal: {signum}\n{frame}\n\n")
+            log_file.write(f"Signal: {signum}\n\n")
+        if signum == signal.SIGUSR2:
+            frame.f_globals["WORK"] = False
+
+    def handler_ctrl_c(signum, frame):
+        with open("signal.log", "a") as log_file:
+            log_file.write("Ctrl + C\n")
+
+
 
     signal.signal(signal.SIGUSR1, handler)
     signal.signal(signal.SIGUSR2, handler)
-    signal.signal(signal.SIGUSR3, handler)
+    signal.signal(signal.SIGINT, handler_ctrl_c)
 
-    while True:
-        try:
+    try:
+        while WORK:
             with open("demon.log", "a") as log_file:
                 log_file.write(f"{datetime.now()}\n\n")
-        except KeyboardInterrupt:
-            with open("signal.log", "a") as log_file:
-                log_file.write("Ctrl + C")
-        except Exception as e:
-            pass
-        finally:
             time.sleep(randint(5, 15))
+    except Exception as e:
+        with open("demon.log", "a") as log_file:
+            log_file.write(f"{e}\n\n")
+    finally:
+        os.unlink(PID_FILE)
+        return
 
 
 
 def start_demon():
-    pid = demon()
+    if os.path.isfile(PID_FILE):
+        with open(PID_FILE, "r") as pid_file:
+            pid = int(pid_file.readline())
+            for process in psutil.process_iter():
+                if process.pid == pid:
+                    print("Demon is working.")
+                    return
+    pid = os.fork()
+    if pid:
+        with open(PID_FILE, "w") as pid_file:
+            pid_file.write(f"{pid}")
+        print("Demon was started.")
+        print(f"Demon has pid: {pid}")
+    else:
+        demon()
+
+def get_pid():
+    pid = 0
+    with open(PID_FILE, "r") as pid_file:
+        pid = int(pid_file.readline())
+    return pid
+
+def send_signal(arg):
+    keys = {
+        "-k": lambda: os.kill(get_pid(), signal.SIGKILL),
+        "-c": lambda: os.kill(get_pid(), signal.SIGINT),
+        "-u": lambda: os.kill(get_pid(), signal.SIGUSR1),
+        "-s": lambda: os.kill(get_pid(), signal.SIGUSR2),
+    }
+    try:
+        print(arg)
+        keys[arg]()
+    except KeyError as e:
+        print(f"Key <{arg}> not found.")
+    return
 
 
-def send_signal(args):
-    pass
 
 
 if __name__ == '__main__':
     try:
-        args = sys.argv[1:]
-    except IndexError:
-        start_demon()
+        os.mkdir(os.path.join(*os.path.split(PID_FILE)[:-1]))
+    except FileNotFoundError:
+        try:
+            os.makedirs(os.path.join(*os.path.split(PID_FILE)[:-1]))
+        except PermissionError:
+            print("WTF !!!")
+            sys.exit(1)
+    except FileExistsError:
+        pass
+    args = sys.argv[1:]
+    if len(args):
+        send_signal(args[0])
     else:
-        send_signal(args)
+        start_demon()
+
